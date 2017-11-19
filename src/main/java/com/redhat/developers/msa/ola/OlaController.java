cb7fb0166c8e882e7ec926f186df12f5fc99f553
smatine <soufiane.matine@gmail.com>
2017-11-19 02:27:46 +0000

    change message

--- src/main/java/com/redhat/developers/msa/ola/OlaController.java
+++ src/main/java/com/redhat/developers/msa/ola/OlaController.java
@@ -47,7 +47,7 @@ public class OlaController {
     @ApiOperation("Returns the greeting in Portuguese")
     public String ola() {
         String hostname = System.getenv().getOrDefault("HOSTNAME", "Unknown");
-        return String.format("Olá de %s", hostname);
+        return String.format("Olá modifié de sma de %s", hostname);
     }
 
     @CrossOrigin
